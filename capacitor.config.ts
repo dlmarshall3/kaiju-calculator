1bd74dac156d49df8b4049225d3c707abb427c61
David Marshall <dlmarshall3@protonmail.com>
2022-10-17 15:06:41 -0500

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'kaiju-calculator',
+  webDir: 'dist/kaiju-calculator',
+  bundledWebRuntime: false
+};
+
+export default config;
